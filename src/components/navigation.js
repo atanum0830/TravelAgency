95841ced34fc2f223e33eeee6fbfd847b3c4a28c
atanum0830 <atanum0830@gmail.com>
2020-12-23 20:37:55 -0500

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,18 +4,17 @@ export function NavBarComponent() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <div className="container-fluid">
+            <div className="container-fluid" style={{maxHeight: "30px"}}>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <a className="navbar-brand" href="/">Monarch Travels</a>
+                <a className="navbar-brand" href="home">Monarch Travels</a>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <div className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <a className="nav-link active" aria-current="page" href="home">Home</a>
-                        <a className="nav-link" href="tours">Tours</a>
-                        <a className="nav-link" href="bookings">Bookings</a>
+                        <a className="nav-link active" aria-current="page" href="tours">Tours</a>
+                        <a className="nav-link active" href="bookings">Bookings</a>
                         <a className="nav-link" href="customers">Customers</a>
-                        <a className="nav-link disabled" href="home" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link disabled" href="home" tabIndex="-1" aria-disabled="true">Disabled</a>
                         <div className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="home">
                             Dropdown link
@@ -31,7 +30,7 @@ export function NavBarComponent() {
 
                 <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-sm btn-secondary" type="button">Search</button>
+                    <button className="mx-2 btn btn-sm btn-secondary" type="button">Search</button>
                 </form>
                 <div className="navbar-nav me-auto mb-2 mb-lg-0">
                     <a className="nav-link active" href="contact">Contact Us</a>
