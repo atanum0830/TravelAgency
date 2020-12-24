15af85df6c9ea931005d6ea8ea664cf1a4318614
atanum0830 <atanum0830@gmail.com>
2020-12-23 20:10:00 -0500

    Navigation First Attempt

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,42 +5,37 @@ export function NavBarComponent() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="home">Monarch Travels</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                <a className="navbar-brand" href="/">Monarch Travels</a>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="home">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="tours">Tours</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="bookings">Bookings</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="customers">Customers</a>
-                        </li>
-                        <li className="nav-item dropdown">
+                    <div className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <a className="nav-link active" aria-current="page" href="home">Home</a>
+                        <a className="nav-link" href="tours">Tours</a>
+                        <a className="nav-link" href="bookings">Bookings</a>
+                        <a className="nav-link" href="customers">Customers</a>
+                        <a className="nav-link disabled" href="home" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <div className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="home">
                             Dropdown link
                             </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="tours">Tours</a></li>
-                                <li><a className="dropdown-item" href="bookings">Bookings</a></li>
-                                <li><a className="dropdown-item" href="customers">Customers</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="home" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <div><a className="dropdown-item" href="tours">Tours</a></div>
+                                <div><a className="dropdown-item" href="bookings">Bookings</a></div>
+                                <div><a className="dropdown-item" href="customers">Customers</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-sm btn-secondary" type="button">Search</button>
+                </form>
+                <div className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <a className="nav-link active" href="contact">Contact Us</a>
+                    <a className="nav-link btn btn-sm btn-secondary" href="login">Login</a>
                 </div>
             </div>
         </nav>
